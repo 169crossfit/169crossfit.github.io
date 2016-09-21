15d930aa1430a93b99465b5dec2d0f4a36217e53
Filipe Pina <fopina@gmail.com>
2016-09-21 23:14:12 +0100

    logo fade

--- js/16.9crossfit.js
+++ js/16.9crossfit.js
@@ -1,6 +1,7 @@
 $('.navbar-main-collapse').bind('hide.bs.collapse', function(){
-  $(".navbar-brand>img").show();
+  $(".navbar-brand").addClass("fade").addClass("in");
 })
 $('.navbar-main-collapse').bind('show.bs.collapse', function(){
-  $(".navbar-brand>img").hide();
+  $(".navbar-brand").removeClass("in");
+  $(".navbar-brand").addClass("fade");
 })
