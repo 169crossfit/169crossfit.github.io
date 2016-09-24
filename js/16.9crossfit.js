51483e817fdcd033c41677262266bd6cbe2633b8
Filipe Pina <fopina@gmail.com>
2016-09-24 13:17:11 +0100

    desktop logo fade fix

--- js/16.9crossfit.js
+++ js/16.9crossfit.js
@@ -2,6 +2,7 @@ $('.navbar-main-collapse').bind('hide.bs.collapse', function(){
   $(".navbar-brand").addClass("fade").addClass("in");
 })
 $('.navbar-main-collapse').bind('show.bs.collapse', function(){
+  if ($('.navbar-nav').width()) return;
   $(".navbar-brand").removeClass("in");
   $(".navbar-brand").addClass("fade");
 })
